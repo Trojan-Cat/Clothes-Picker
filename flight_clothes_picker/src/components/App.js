e2f97c7cf94198bebef0879207ebbbf5a4bd0026
TrojinCat <kajarrod@gmail.com>
2019-05-26 11:25:02 +1000

    Adding in state picker and date

--- flight_clothes_picker/src/components/App.js
+++ flight_clothes_picker/src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "../apis/OpenWeatherMap";
 
+import Selection from "./Selection";
+
 const key = "56f09b962e8a014f246bffc48f44b792";
 const id = "7299965";
 const day = "2";
@@ -16,7 +18,16 @@ axios
   });
 
 const App = () => {
-  return <div className="ui container">Stuff</div>;
+  return (
+    <div className="ui container">
+      <div>
+        <h1 className="ui  center aligned header">
+          Select your flight details
+        </h1>
+      </div>
+      <Selection />
+    </div>
+  );
 };
 
 export default App;
---
+++ flight_clothes_picker/src/components/Selection.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+class Selection extends React.Component {
+  state = {
+    startDate: "",
+    startLoc: "asd",
+    endDate: "",
+    endLoc: ""
+  };
+
+  //TODO: Change this to highlight the filed that needs to  be filled in rather than alerts
+  onGo = event => {
+    const startDate = this.state.startDate;
+    const startLoc = this.state.startLoc;
+    const endDate = this.state.endDate;
+    const endLoc = this.state.endLoc;
+
+    if (startDate === "") {
+      alert("Select a start date");
+    } else if (startLoc === "") {
+      alert("Select a start location");
+    } else if (endDate === "") {
+      alert("Select a start date");
+    } else if (endLoc === "") {
+      alert("Select a end location");
+    }
+  };
+
+  render() {
+    return (
+      <div className="ui menu">
+        <div className="item">
+          <div className="ui icon input icon">
+            <input
+              type="date"
+              value={this.state.startDate}
+              onChange={e => this.setState({ startDate: e.target.valueAsDate })}
+            />
+            <input
+              type="text"
+              placeholder="Leaving from..."
+              value={this.state.startLoc}
+              onChange={e => this.setState({ startLoc: e.target.value })}
+            />
+            <i aria-hidden="true" className="search icon" />
+          </div>
+        </div>
+        <div className="right item">
+          <div className="ui action input">
+            <input
+              type="date"
+              id="arriveDate"
+              value={this.state.endDate}
+              onChange={e => this.setState({ endDate: e.target.valueAsDate })}
+            />
+            <input
+              type="text"
+              placeholder="Arriving at..."
+              value={this.state.endLoc}
+              onChange={e => this.setState({ endLoc: e.target.value })}
+            />
+            <button type="submit" className="ui button">
+              Go
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Selection;
