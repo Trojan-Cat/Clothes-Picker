7db8eb321b98719f64ce54477e5d6e591b8b6e37
TrojinCat <kajarrod@gmail.com>
2019-05-30 17:34:43 +1000

    Change of name of directory

--- clothes-picker/src/apis/OpenWeatherMap.js
+++ clothes-picker/src/apis/OpenWeatherMap.js
@@ -1,11 +1,5 @@
 import axios from "axios";
 
-/*
-const key = "56f09b962e8a014f246bffc48f44b792";
-const id = "7299965";
-const day = "2";
-*/
-
 export default axios.create({
   baseURL: `http://api.openweathermap.org/data/2.5/forecast`
 });
